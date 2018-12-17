aab5809bcc78a9beba4bca5ed36ecbb68f4e4956
Taylor Munroe <t.munroe@aol.co.uk>
2018-12-17 23:58:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!"
-}
-
-function happyHolidaysTo(name){
-  return "Happy holidays, you!"
-}
-
-function happyHolidayTo(holiday){
-  return "Happy ${holiday}, you!"
-}
-
-function holidayCountdown('holiday', 'days'){
-  return "It\'s ${days} days until ${holiday}!"
+  return "Happy holidays!";
 }
